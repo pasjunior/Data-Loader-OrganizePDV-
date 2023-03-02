20cc67821e7b3bd4d6656d8ca326b311a2dc9601
pasjunior <paulo.asjr@hotmail.com>
2023-03-01 21:34:29 -0300

    Otimização na tabela Unidade e NCM

--- script/Gdoor.py
+++ script/Gdoor.py
@@ -5,7 +5,11 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 import pandas as pd
+import numpy as np
+import os
 import requests
+import time
+
 
 #Selecionando arquivo do banco de dados
 
@@ -22,6 +26,7 @@ def select_file():
 
 file_path = select_file()
 
+
 # Conectando com o banco Firebird
 
 con = fdb.connect(
@@ -33,6 +38,7 @@ con = fdb.connect(
 
 cur = con.cursor() # Cria objeto usado para realizar operações em um banco de dados
 
+
 # Faz um select na tabela selecionada do banco de dados para UND_VENDA
 
 cur.execute("SELECT DISTINCT UND FROM ESTOQUE")
@@ -40,7 +46,17 @@ cur.execute("SELECT DISTINCT UND FROM ESTOQUE")
 rows = cur.fetchall()
 print(rows)
 
-dePara = pd.read_excel('C:/Paulo/Matheus/OrganizePDV-main/dePara_unidade.xlsx', sheet_name='UNIDADE')
+# Verificar se o arquivo 'dePara_unidade.xlsx' existe
+
+if not os.path.exists('dePara_unidade.xlsx'):
+    # Se o arquivo não existir, criar um novo arquivo com uma planilha 'UNIDADE'
+    dePara = pd.DataFrame({'de': [], 'para': []})
+    writer = pd.ExcelWriter('dePara_unidade.xlsx', engine='xlsxwriter')
+    dePara.to_excel(writer, sheet_name='UNIDADE', index=False)
+    writer.save()
+else:
+    # Se o arquivo existir, ler o arquivo e carregar os dados em um DataFrame
+    dePara = pd.read_excel('dePara_unidade.xlsx', sheet_name='UNIDADE')
 
 dePara = pd.DataFrame(dePara)
 print(dePara)
@@ -67,8 +83,8 @@ for row in divergencia:
 
 # Escreve no arquivo excel
 
-dePara.to_excel('C:/Paulo/Matheus/OrganizePDV-main/dePara_unidade.xlsx',
-                sheet_name='UNIDADE', index=False)
+#dePara.to_excel('C:/Paulo/Matheus/OrganizePDV-main/dePara_unidade.xlsx', sheet_name='UNIDADE', index=False)
+dePara.to_excel('dePara_unidade.xlsx', sheet_name='UNIDADE', index=False)
 
 
 # Procura se existe registros em branco
@@ -86,7 +102,7 @@ cur.execute("SELECT DISTINCT UND_COMPRA FROM ESTOQUE")
 rows = cur.fetchall()
 print(rows)
 
-dePara = pd.read_excel('C:/Paulo/Matheus/OrganizePDV-main/dePara_unidade.xlsx', sheet_name='UNIDADE')
+dePara = pd.read_excel('dePara_unidade.xlsx', sheet_name='UNIDADE')
 
 dePara = pd.DataFrame(dePara)
 print(dePara)
@@ -112,8 +128,7 @@ for row in divergencia:
 
 # Escreve no arquivo excel
 
-dePara.to_excel('C:/Paulo/Matheus/OrganizePDV-main/dePara_unidade.xlsx',
-                sheet_name='UNIDADE', index=False)
+dePara.to_excel('dePara_unidade.xlsx',sheet_name='UNIDADE', index=False)
 
 
 # Procura se existe registros em branco
@@ -131,31 +146,50 @@ cur.execute("SELECT CODIGO, BARRAS, DESCRICAO, UND, UND_COMPRA, COD_CEST, COD_NC
 rows = cur.fetchall()
 
 dados_payload = []
+mensagens = []
+dfErros = []
+
+# lendo tabela Unidade OrganizePDV
+
+url = f"http://127.0.0.1:8090/api/ControllerUnidade?pagesize=9999999999&page=1"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+respostaJson_unidade = response.json()
+itemsJson_unidade = respostaJson_unidade['items']
+
+# lendo tabela NCM OrganizePDV
+
+url = f"http://127.0.0.1:8090/api/ControllerNCM?pagesize=9999999999&page=1"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+respostaJson_ncm = response.json()
+itemsJson_ncm = respostaJson_ncm['items']
+    
 
 for row in rows:
 # UND_VENDA
     und_venda = 'EM BRANCO' if row[3] is None else row[3]
     if (dePara['de'] == und_venda).any():
-        sigla_und_venda = dePara.loc[dePara['de'] == und_venda, 'para'].iloc[0]
-    else: 
-        messagebox.showinfo("Informação", f'Unidade de venda {und_venda} não existe no dePara')
-        exit(0)
-    
-      
-    url = f"http://127.0.0.1:8090/api/ControllerUnidade?csigla={sigla_und_venda}"
-
-    payload={}
-    headers = {}
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    respostaJson = response.json()
-    itemsJson = respostaJson['items']
-    if len(itemsJson) > 0:
-        unidade_venda = itemsJson[0]
+        sigla_und_compra = dePara.loc[dePara['de'] == und_venda, 'para'].iloc[0]
     else:
-        messagebox.showinfo("Informação", f'Unidade de venda {sigla_und_venda} não existe no PDV')
-        exit(0)
+        dfErros.append({'codigo': row[0], 'descrição': row[2], 'divergencia': f'Unidade de venda {und_venda} não existe no dePara'})
+        continue
+    
+    unidade_compra = {}
+    for unidade in itemsJson_unidade:
+        if unidade["CSIGLA"] == sigla_und_compra:
+            unidade_compra = unidade
+    if not unidade_compra:
+        dfErros.append({'codigo': row[0], 'descrição': row[2], 'divergencia': f'Unidade de venda {sigla_und_compra} não existe no PDV'})
+        continue
 
 # UND_COMPRA
     und_compra = 'EM BRANCO' if row[4] is None else row[4]
@@ -163,48 +197,52 @@ for row in rows:
     if (dePara['de'] == und_compra).any():
         sigla_und_compra = dePara.loc[dePara['de'] == und_compra, 'para'].iloc[0]
     else: 
-        messagebox.showinfo("Informação", f'Unidade de Compra {und_compra} não existe no dePara')
-        exit(0)
+        dfErros.append({'codigo': row[0], 'descrição': row[2], 'divergencia': f'Unidade de Compra {und_compra} não existe no dePara'})
+        continue
+
+    unidade_compra = {}
+    for unidade in itemsJson_unidade:
+        if unidade["CSIGLA"] == sigla_und_compra:
+            unidade_compra = unidade
+    if not unidade_compra:
+        dfErros.append({'codigo': row[0], 'descrição': row[2], 'divergencia': f'Unidade de Compra {sigla_und_compra} não existe no PDV'})
+        continue
+
+# NCM
+    cod_ncm = row[6]
+    ncm = {}
+    for ncmpdv in itemsJson_ncm:
+        if ncmpdv["CNCM"] == cod_ncm:
+            ncm = ncmpdv
+    if not ncm:
+        dfErros.append({'codigo': row[0], 'descrição': row[2], 'divergencia': f'Código NCM {cod_ncm} não existe no PDV'})
+        continue
     
-      
-    url = f"http://127.0.0.1:8090/api/ControllerUnidade?csigla={sigla_und_compra}"
-
-    payload={}
-    headers = {}
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    respostaJson = response.json()
-    itemsJson = respostaJson['items']
-    if len(itemsJson) > 0:
-        unidade_compra = itemsJson[0]
-    else:
-        messagebox.showinfo("Informação", f'Unidade de Compra {sigla_und_compra} não existe no PDV')
-        exit(0)
-
-
     payload = {
         #"CCDVE": row[0],
         "CCODBAR": row[1], #OK
         "CDESCRICAO": row[2], #OK
-        "O1UM-NID": unidade_venda['NID'], #OK
+        "O1UM-NID": unidade_compra['NID'], #OK
         "O2UM-NID": unidade_compra['NID'], #OK
-        "ONCM-OCEST-NID": row[5],
-        "ONCM-NID": row[6],
+        "ONCM-NID": ncm['NID'],
         "CTIPO": row[7], #OK
         "OTE-NID": 1,
         "OTS-NID": 2,
         "NPLIQ": float(row[9]), # Tive que converter em float pq a biblioteca requests n reconhece números decimais
-        #"COBS": row[10],
-    }
+        "NPBRU": float(row[9])
+}
   
 
     dados_payload.append(payload)
+       
 
+if len(dfErros) > 0:
+    dfErros = pd.DataFrame(dfErros)
+    dfErros.to_excel('C:/Paulo/Matheus/OrganizePDV-main/erros.xlsx', sheet_name='ERROS', index=False)
+    messagebox.showinfo("Atenção", f'Tratar os erros em: erros.xlsx')
+    
 print(dados_payload)
-
 exit(0)
-
 
 for payload in dados_payload:
     url = "http://127.0.0.1:8090/api/ControllerProduto" # Conectando com o PDV
